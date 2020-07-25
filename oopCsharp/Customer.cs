aa487dd570411c18e07c287066f7f75a21e0dba6
Caio Augusto <caioag42@gmail.com>
2020-07-25 15:39:29 -0300

    adding unit testing

--- oopCsharp/Customer.cs
+++ oopCsharp/Customer.cs
@@ -4,6 +4,27 @@ namespace oopCsharp
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
 
+        private string _lastName;
+        public string LastName { get; set; }
     }
 }
---
+++ oopCsharpTests/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using oopCsharp;
+using Xunit;
+
+namespace oopCsharpTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ExpectedName()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameFirstNameEmpty()
+        {
+            Customer customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+            string expected = "Baggins";
+
+            string actual = customer.FullName;
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+            };
+            string expected = "Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
